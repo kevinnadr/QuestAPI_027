33737d35c7d78f8b3cd1905b6c379eb83a41a330
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-19 20:41:31 +0700

    membuat file entryviewmodel dan membuat kode yang berfungsi untuk mengelola data dan logika untuk layar entri data siswa (EntrySiswaScreen).

---
+++ app/src/main/java/com/example/questapi_027/viewmodel/EntryViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.questapi_027.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.questapi_027.modeldata.DetailSiswa
+import com.example.questapi_027.modeldata.UIStateSiswa
+import com.example.questapi_027.modeldata.toDataSiswa
+import com.example.questapi_027.repositori.RepositoryDataSiswa
+
+class EntryViewModel(
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    // Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: retrofit2.Response<Void> =
+                repositoryDataSiswa.postDataSiswa(
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            } else {
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
+}
