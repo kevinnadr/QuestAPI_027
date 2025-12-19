534f23566352d07408a1d77b0f5998f8a2471936
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-19 20:45:37 +0700

    membuat file destinasihome dan membuat kode yang berfungsi untuk mendefinisikan destinasi navigasi untuk layar utama (Home) aplikasi.

---
+++ app/src/main/java/com/example/questapi_027/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_027.uicontroller.route
+
+import com.example.questapi_027.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "Home"
+    override val titleRes = R.string.app_name
+}
