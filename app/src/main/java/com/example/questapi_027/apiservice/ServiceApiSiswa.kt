4f6f52412e0dcd5955a1af00c19a1fdb612447f3
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-20 07:34:25 +0700

    mengubah , pada insertTM,php menjadi insertTM.php

--- app/src/main/java/com/example/questapi_027/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_027/apiservice/ServiceApiSiswa.kt
@@ -6,9 +6,9 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ServiceApiSiswa{
-    @GET("bacaTeman.php")
+    @GET("bacateman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-    @POST("insertTM,php")
+    @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
