d85bffbd010e16ad641bb8b9dcb47fe4cc46ece8
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-26 22:57:30 +0700

    menambahkan object untuk DestinasiDetail.kt

--- app/src/main/java/com/example/questapi_027/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_027/uicontroller/route/DestinasiDetail.kt
@@ -1,3 +1,11 @@
 package com.example.questapi_027.uicontroller.route
 
 
+import com.example.questapi_027.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
