8390df83f9275cf18910c40e3225fa1d7402e88e
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-26 22:53:43 +0700

    membuat suspenfund untuk melihat data satu siswa menghapus dan mengedit

--- app/src/main/java/com/example/questapi_027/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_027/repositori/RepositoryDataSiswa.kt
@@ -2,26 +2,32 @@ package com.example.questapi_027.repositori
 
 import com.example.questapi_027.apiservice.ServiceApiSiswa
 import com.example.questapi_027.modeldata.DataSiswa
+import retrofit2.Response
 
 
 interface RepositoryDataSiswa {
 
     suspend fun getDataSiswa(): List<DataSiswa>
 
-    suspend fun postDataSiswa(
-        dataSiswa: DataSiswa
-    ): retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ) : RepositoryDataSiswa {
 
-    override suspend fun getDataSiswa(): List<DataSiswa> =
-        serviceApiSiswa.getSiswa()
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
 
-    override suspend fun postDataSiswa(
-        dataSiswa: DataSiswa
-    ): retrofit2.Response<Void> =
-        serviceApiSiswa.postSiswa(dataSiswa)
-}
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+
+
+
+    }
+
+
