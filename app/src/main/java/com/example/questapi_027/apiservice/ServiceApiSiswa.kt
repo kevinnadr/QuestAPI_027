bc6745272973a8d86fdd9f366f531692607501b9
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-27 07:31:42 +0700

    memperbaiki typo baca1teman menjadi bacateman

--- app/src/main/java/com/example/questapi_027/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_027/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.questapi_027.apiservice
 
+
 import com.example.questapi_027.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -8,19 +9,20 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
 
-interface ServiceApiSiswa{
-    @GET("bacateman.php")
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    @GET("bacalTeman.php/{id]")
-    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+    @GET("bacaTeman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id : Int): List<DataSiswa>
 
-    @PUT("editTM.php/{id}")
-    suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    @PUT("editTM.php")
+    suspend fun editSatuSiswa(@Query("id")id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    @DELETE("deleteTM.php/{id}")
-    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
+    @DELETE("DeleteTM.php")
+    suspend fun hapusSatuSiswa(@Query("id")id:Int): retrofit2.Response<Void>
 }
